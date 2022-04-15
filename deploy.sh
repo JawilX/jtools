a9b7b2fbd8bbe342dd142efe023e0dde2fb7d536
Jawil <xjh53047@gmail.com>
2022-04-15 17:43:30 +0800

    feat: add deploy to github pages

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# Github Pages 部署文档：https://cn.vitejs.dev/guide/static-deploy.html#github-pages
+
+# 发生错误时中止
+# set -e
+
+# 构建
+npm run build
+
+# 进入构建文件
+cd dist
+
+git init
+git checkout -b gh-pages
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:JawilX/jtools.git gh-pages
+
+cd -
