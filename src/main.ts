6f2aeb2ab4eef5c76f480af63e99577e8659f8ca
Jawil <xjh53047@gmail.com>
2022-04-21 17:27:11 +0800

    fix: handle tailwind style override naive-ui issue

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import '@/plugins/day'
 import './assets/styles/index.css'
 import 'normalize.css'
 
+const meta = document.createElement('meta')
+meta.name = 'naive-ui-style'
+document.head.appendChild(meta)
+
 const app = createApp(App)
 app.config.globalProperties.window = window
 app.use(router).use(naive).mount('#app')
