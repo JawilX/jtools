2ef3c530e5bbdd6e5e521a494646f74652bec3f2
Jawil <xjh53047@gmail.com>
2022-04-22 15:05:04 +0800

    feat: update Deduplication

--- src/views/Deduplication.vue
+++ src/views/Deduplication.vue
@@ -1,30 +1,28 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { NInput, NButton, NSpace } from 'naive-ui'
 import { copyText } from '@/utils'
 import BaseTitle from '@/components/BaseTitle.vue'
 
 const input = ref('')
 
-const output = ref('')
-
-function deduplication() {
+const output = computed(() => {
   const s = input.value.split(/\r?\n/).filter(i => i)
   const m = new Map<string, number>()
   for (const i of s) {
     const iv = m.get(i)
     m.set(i, iv ? iv + 1 : 1)
   }
-  output.value = ''
+  let result = ''
   m.forEach((v, k) => {
-    output.value += `${k}  // 出现了：${v}次\n`
+    result += `${k}  // 出现了：${v}次\n`
   })
-  output.value = output.value.slice(0, -1)
-}
+  result = result.slice(0, -1)
+  return result
+})
 
 function clear() {
   input.value = ''
-  output.value = ''
 }
 </script>
 
@@ -32,7 +30,6 @@ function clear() {
   <BaseTitle />
   <NInput v-model:value="input" type="textarea" :rows="10" placeholder="输入需要统计的内容" />
   <NSpace class="py-2" justify="center">
-    <NButton secondary type="info" @click="deduplication">开始统计</NButton>
     <NButton secondary type="info" @click="copyText(output)">复制结果</NButton>
     <NButton secondary type="info" @click="clear">清空</NButton>
   </NSpace>
